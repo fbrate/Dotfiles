3d448d25211432710f54ce23314698096205af44
Frithjof Brate <fbrate@outlook.com>
2019-10-15 11:31:03 +0200

    update

--- NightAutoChange/swapSolarizedColor.sh
+++ NightAutoChange/swapSolarizedColor.sh
@@ -13,13 +13,13 @@ if [ "Dark" == "$load_or_create" ]; then
 	cp ./src/xfce4-term-color-pallete-dark ~/.config/xfce4/terminal/terminalrc
 	sed -i '/set background=/c\set background=dark' ~/.vimrc
 	sed -i '/let g:airline_solarized_bg=/c\let g:airline_solarized_bg="dark"' ~/.vimrc
-#	sed -i '/gtk=theme-name=/c\gtk-theme-name=NumixSolarizedDarkBlue' "$HOME/.gtkrc-2.0.mine"
-
+	sed -i '/gtk=theme-name=/c\gtk-theme-name=NumixSolarizedDarkBlue' "$HOME/.gtkrc-2.0.mine"
+	sed -i '/gtk=theme-name=/c\gtk-theme-name=NumixSolarizedDarkBlue' "$HOME/.config/gtk-3.0/settings.ini"
 elif [ "Light" == "$load_or_create" ]; then
 
 	cp ./src/xfce4-term-color-pallete-light ~/.config/xfce4/terminal/terminalrc
 	sed -i '/set background=/c\set background=light' ~/.vimrc
 	sed -i '/let g:airline_solarized_bg=/c\let g:airline_solarized_bg="light"' ~/.vimrc
-#	sed -i '/gtk=theme-name=/c\gtk-theme-name=NumixSolarizedLightBlue' "$HOME/.gtkrc-2.0.mine"
-
+	sed -i '/gtk=theme-name=/c\gtk-theme-name=NumixSolarizedLightBlue' "$HOME/.gtkrc-2.0.mine"
+ 	sed -i '/gtk=theme-name=/c\gtk-theme-name=NumixSolarizedLightBlue' "$HOME/.config/gtk-3.0/settings.ini"
 fi
